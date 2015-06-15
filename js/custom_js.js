7220eeb5ff3dc3aabdc8b0195e8abf77012515a3
Fabio Santana <fabio.scienvitarso@gmail.com>
2015-06-14 23:41:35 -0300

    ajust layout and insert search

--- js/custom_js.js
+++ js/custom_js.js
@@ -1,4 +1,16 @@
+/// <reference path="../typings/jquery/jquery.d.ts"/>
 // Initialize collapse button
   $(".button-collapse").sideNav();
   // Initialize collapsible (uncomment the line below if you use the dropdown variation)
   //$('.collapsible').collapsible();
+  
+  
+$(document).ready(function(){
+    $("#search-nav").hide();
+    $("#search-hide-custom").click(function(){
+        $("#search-nav").hide(500);
+    });
+    $("#search-show-custom").click(function(){
+        $("#search-nav").show(500);
+    });
+});
